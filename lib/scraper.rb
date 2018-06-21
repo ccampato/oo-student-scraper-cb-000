176e79da11eb77afd900efdf5b7ba6cea0a7de08
Caio Sanches <ccampato@gmail.com>
2018-06-21 23:32:51 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,7 @@ class Scraper
     student[:twitter] = doc.css('a').map { |e| e.attributes.values.first.value }.detect {|e| e =~ /twitter.com/ } unless doc.css('a').map { |e| e.attributes.values.first.value }.detect {|e| e =~ /twitter.com/ } == nil
     student[:linkedin] = doc.css('a').map { |e| e.attributes.values.first.value }.detect {|e| e =~ /linkedin.com/ } unless doc.css('a').map { |e| e.attributes.values.first.value }.detect {|e| e =~ /linkedin.com/ } == nil
     student[:github] = doc.css('a').map { |e| e.attributes.values.first.value }.detect {|e| e =~ /github.com/ } unless doc.css('a').map { |e| e.attributes.values.first.value }.detect {|e| e =~ /github.com/ } == nil
-    student[:blog] = doc.css(".social-icon-container a").last.attribute("href").value unless doc.css(".social-icon-container a")[3] == nil
+    student[:blog] = doc.css(".social-icon-container a").last.attribute("href").value unless doc.css(".social-icon-container a").last == nil
     student[:profile_quote] = doc.css(".profile-quote").text
     student[:bio] = doc.css(".description-holder p").text
     student
